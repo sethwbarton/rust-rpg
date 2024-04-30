ffdd04cd1b542610bb5f0bf1d7c1c52891b1b019
sethbarton <seth.barton@integral.io>
2024-04-30 09:24:21 -0600

    Use nannou

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
-use rust_rpg::lib::run;
+use nannou::prelude::*;
 
 fn main() {
-    run();
+    nannou::app(model)
+        .update(update)
+        .simple_window(view)
+        .run();
+}
+
+struct Model {}
+
+fn model(_app: &App) -> Model {
+    Model {}
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {
+}
+
+fn view(_app: &App, _model: &Model, frame: Frame){
+    frame.clear(PURPLE);
 }
