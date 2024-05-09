0dbb128e5f911d363f86f58e897a097df12e40f3
sethbarton <seth.barton@integral.io>
2024-05-09 08:35:21 -0600

    Lots of wrangling this UI flow...

--- src/ui/graphics/mod.rs
+++ src/ui/graphics/mod.rs
@@ -1,10 +1,9 @@
 use crate::game::drawable::Drawable;
 use crate::game::game_state::game_state::GameState;
-use crate::ui::app_view::View;
 use nannou::geom::Vec3;
 use nannou::{App, Frame};
 
-pub fn draw_scene(_app: &App, _model: &GameState, frame: &Frame, view: &View) {
+pub fn draw_scene(_app: &App, _model: &GameState, frame: &Frame) {
     let scene_draw_instance = _app.draw().scale(_model.scale as f32).translate(Vec3::new(
         _model.transform.x,
         _model.transform.y,
--- src/ui/gui/mod.rs
+++ src/ui/gui/mod.rs
@@ -1,32 +1,21 @@
 pub mod components;
 
 use crate::game::game_state::game_state::GameState;
-use crate::ui::app_view::{RenderableView, View};
 use crate::ui::gui::components::ship_health::ShipHealthUi;
 use nannou::geom::Point2;
 use nannou::{App, Draw, Frame};
 
 pub trait ClickableGuiElement {
     fn on_click(self: &Self, app: &App, state: &GameState) -> ();
-    // Check if x_y is inside the gui element
     fn is_in(self: &Self, x_y: Point2) -> bool;
     fn draw(self: &Self, app: &App, state: &GameState, draw_instance: &Draw) -> ();
 }
 
-pub fn draw_ui(
-    _app: &App,
-    state: &GameState,
-    frame: &Frame,
-    gui_elements: &Vec<Box<dyn ClickableGuiElement>>,
-    view: &View,
-) {
-    // One thing I'm learning about Rust.... it might be better to write more straightforward code.
-    // No dynamic dispatch.
-    view.register_gui_element(ShipHealthUi {});
+pub fn draw_ui(_app: &App, state: &GameState, frame: &Frame) {
+    let ui_draw_instance = _app.draw().scale(1.0);
 
-    for element in gui_elements {
-        let ui_draw_instance = _app.draw().scale(2.0);
-        element.draw(_app, state, &ui_draw_instance);
-        ui_draw_instance.to_frame(_app, &frame).unwrap();
-    }
+    let ship_health = ShipHealthUi {};
+    ship_health.draw(_app, state, &ui_draw_instance);
+
+    ui_draw_instance.to_frame(_app, &frame).unwrap();
 }
